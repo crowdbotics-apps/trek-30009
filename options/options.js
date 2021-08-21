47a7ed52adbba0abd15f9b4a22dd24abd99b17c6
Crowdbotics <team@crowdbotics.com>
2021-08-21 19:07:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "trek_30009Identifier",
+  url: "https://trek_30009Identifier.botics.co",
+  api: "https://trek_30009Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
